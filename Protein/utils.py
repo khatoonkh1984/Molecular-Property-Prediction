ec466e6375835957b367e4bab7028be9c7dfa0d2
khatoon khedri <102431781+khatoonkh1984@users.noreply.github.com>
2023-10-14 15:29:11 +0330

    Add files via upload

---
+++ Protein/utils.py
@@ -0,0 +1,122 @@
+import itertools
+import os
+
+os.environ["DGLBACKEND"] = "pytorch"
+
+import copy
+import dgl
+import dgl.data
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.prune as prune
+from sklearn.metrics import roc_auc_score
+from torchprofile import profile_macs
+
+
+def get_model_macs(model, inputs) -> int:
+    '''
+    MACs are a common metric to measure the computational complexity 
+    of deep neural networks, as they reflect the number of arithmetic
+    operations involved in the forward pass of the model
+    '''
+    return profile_macs(model, inputs)
+
+
+def get_sparsity(tensor: torch.Tensor) -> float:
+    """
+    calculate the sparsity of the given tensor
+        sparsity = #zeros / #elements = 1 - #nonzeros / #elements
+    """
+    return 1 - float(tensor.count_nonzero()) / tensor.numel()
+
+
+def get_model_sparsity(model: nn.Module) -> float:
+    """
+    calculate the sparsity of the given model
+        sparsity = #zeros / #elements = 1 - #nonzeros / #elements
+    """
+    num_nonzeros, num_elements = 0, 0
+    for param in model.parameters():
+        num_nonzeros += param.count_nonzero()
+        num_elements += param.numel()
+    return 1 - float(num_nonzeros) / num_elements
+
+def get_num_parameters(model: nn.Module, count_nonzero_only=False) -> int:
+    """
+    calculate the total number of parameters of model
+    :param count_nonzero_only: only count nonzero weights
+    """
+    num_counted_elements = 0
+    for param in model.parameters():
+        if count_nonzero_only:
+            num_counted_elements += param.count_nonzero()
+        else:
+            num_counted_elements += param.numel()
+    return num_counted_elements
+
+
+def get_model_size(model: nn.Module, data_width=32, count_nonzero_only=False) -> int:
+    """
+    calculate the model size in bits
+    :param data_width: #bits per element
+    :param count_nonzero_only: only count nonzero weights
+    """
+    return get_num_parameters(model, count_nonzero_only) * data_width
+
+Byte = 8
+KiB = 1024 * Byte
+MiB = 1024 * KiB
+GiB = 1024 * MiB
+
+def fine_grained_prune(tensor: torch.Tensor, sparsity : float) -> torch.Tensor:
+    """
+    magnitude-based pruning for single tensor
+    :param tensor: torch.(cuda.)Tensor, weight of conv/fc layer
+    :param sparsity: float, pruning sparsity
+        sparsity = #zeros / #elements = 1 - #nonzeros / #elements
+    :return:
+        torch.(cuda.)Tensor, mask for zeros
+    """
+    sparsity = min(max(0.0, sparsity), 1.0)
+    if sparsity == 1.0:
+        tensor.zero_()
+        return torch.zeros_like(tensor)
+    elif sparsity == 0.0:
+        return torch.ones_like(tensor)
+
+    num_elements = tensor.numel()
+
+    num_zeros = round(num_elements * sparsity)
+    importance = tensor.abs()
+    threshold = importance.view(-1).kthvalue(num_zeros).values
+    mask = torch.gt(importance, threshold)
+    tensor.mul_(mask)
+
+    return mask
+
+class FineGrainedPruner:
+    def __init__(self, model, sparsity_dict):
+        self.masks = FineGrainedPruner.prune(model, sparsity_dict)
+
+    @torch.no_grad()
+    def apply(self, model):
+        for name, param in model.named_parameters():
+            if name in self.masks:
+                param *= self.masks[name]
+
+    @staticmethod
+    @torch.no_grad()
+    def prune(model, sparsity_dict):
+        masks = dict()
+        for name, param in model.named_parameters():
+            if param.dim() > 1: # we only prune conv and fc weights
+                if isinstance(sparsity_dict, dict):
+                    masks[name] = fine_grained_prune(param, sparsity_dict[name])
+                else:
+                    assert(sparsity_dict < 1 and sparsity_dict >= 0)
+                    if sparsity_dict > 0:
+                        masks[name] = fine_grained_prune(param, sparsity_dict)
+        return masks
